3afa1d45d59c79c2214bcb6517345ed879ae7a84
daanrongen <daanrongen@hotmail.com>
2018-04-19 23:16:28 +0200

    add local server that servers logEntries for the ontwerpbiografie, displaying a list page and detail pages per logEntry

--- log/server/index.js
+++ log/server/index.js
@@ -1,15 +1,55 @@
 'use strict'
 
 var express = require('express')
+var mongo = require('mongodb')
+var ObjectID = require('mongodb').ObjectID
+
+require('dotenv').config()
+
+var db = null
+var url = 'mongodb://' + process.env.DB_HOST + ':' + process.env.DB_PORT
+
+mongo.MongoClient.connect(url, function (err, client) {
+  if (err) {
+    throw err
+  }
+  db = client.db(process.env.DB_NAME)
+  console.log('connected with mongodb, databasename:', db.databaseName)
+})
 
 express()
   .use(express.static('static'))
   .set('view engine', 'ejs')
   .set('views', 'view')
   .get('/', all)
+  .get('/:id', detail)
   .listen(4000, console.log('listening on http:localhost:4000'))
 
 function all(req, res) {
   res.status(200)
-  res.render('list.ejs')
+  var logEntries = db.collection('logEntries')
+
+  logEntries.find().toArray(function (err, results) {
+    if (err) {
+      res.render('error.ejs', {id: err.id, title: err.title, description: err.description})
+    } else {
+      res.render('list.ejs', {data: results})
+    }
+  })
+}
+
+function detail(req, res) {
+  var id = new ObjectID(req.params.id)
+
+  db.collection('logEntries').findOne({
+    _id: id
+  }, done)
+
+  function done(err, data) {
+    if (err) {
+      res.render('error.ejs', {id: err.id, title: err.title, description: err.description})
+    } else {
+      res.render('detail.ejs', {data: data})
+    }
+  }
 }
