d66460ab91307d5a474a29a8164337ddb0ad4830
daanrongen <daanrongen@hotmail.com>
2018-04-22 11:05:29 +0200

    edit rendered template to render detailed errors, list of all database objects, form to add data to the database and a detailed view per database item

--- log/server/index.js
+++ log/server/index.js
@@ -3,6 +3,8 @@
 var express = require('express')
 var mongo = require('mongodb')
 var ObjectID = require('mongodb').ObjectID
+var date = require('date-and-time')
+var bodyParser = require('body-parser')
 
 require('dotenv').config()
 
@@ -19,10 +21,14 @@ mongo.MongoClient.connect(url, function (err, client) {
 
 express()
   .use(express.static('static'))
+  .use(bodyParser.urlencoded({extended: false}))
+  .use(bodyParser.json())
   .set('view engine', 'ejs')
   .set('views', 'view')
   .get('/', all)
+  .get('/add', form)
   .get('/:id', detail)
+  .post('/add', addLogEntry)
   .listen(4000, console.log('listening on http:localhost:4000'))
 
 function all(req, res) {
@@ -50,6 +56,35 @@ function detail(req, res) {
       res.render('error.ejs', {id: err.id, title: err.title, description: err.description})
     } else {
       res.render('detail.ejs', {data: data})
+    }
+  }
+}
+
+function form(req, res) {
+  try {
+    res.status(200).render('form.ejs')
+  } catch (err) {
+    res.render('error.ejs', {id: err.id, title: err.title, description: err.description})
+  }
+}
+
+function addLogEntry(req, res) {
+  var now = new Date()
+  var logEntries = db.collection('logEntries')
+
+  logEntries.insertOne({
+    dateAdded: date.format(now, 'ddd D MMMM'),
+    title: req.body.title,
+    goal: req.body.goal,
+    tags: [req.body.tagOne, req.body.tagTwo, req.body.tagThree, req.body.tagFour],
+    text: req.body.text
+  }, done)
+
+  function done(err, data) {
+    if (err) {
+      res.render('error.ejs', {id: err.id, title: err.title, description: err.description})
+    } else {
+      res.redirect('/' + data.insertedId)
     }
   }
 }
