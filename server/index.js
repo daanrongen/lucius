2dbef5df4b2ecf12c5c9e42aaa34406d9642ed28
daanrongen <daanrongen@hotmail.com>
2018-06-04 21:17:28 +0200

    setup local server and install basic dependencies

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
 'use strict'
+
+var express = require('express')
+
+express()
+  .use(express.static('static'))
+  .get('/', all)
+  .listen(4000, console.log('listening on localhost:4000'))
+
+function all(req, res) {
+  res.status(200)
+  console.log('visitor arrived')
+}
