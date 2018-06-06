72b6b8f3a75c2378219eea8d98d8f0021fa96feb
daanrongen <daanrongen@hotmail.com>
2018-06-06 16:23:01 +0200

    server is live

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@
 import routes from '../src/routes/crm-routes'
 
 const express = require('express')
+const session = require('express-session')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
@@ -12,18 +13,23 @@ const port = 4000
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://localhost/CRMdb')
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
-
 routes(app)
 
-app.use(express.static('public'))
-
-app.get('/', all)
+app
+  .use(express.static('public'))
+  .use(bodyParser.urlencoded({extended: true}))
+  .use(bodyParser.json())
+  .use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+  }))
+  .set('view engine', 'ejs')
+  .set('views', 'public')
+  .get('/', all)
   .listen(port, console.log('listening on localhost:', port))
 
 function all(req, res) {
   res.status(200)
-  console.log(req.session)
-  res.send('<h1>Project Delta</h1>' + '<h2>beta</h2>' + '<form action="/users" method="post">' + '<input type="text" name="username" placeholder="username" autofocus>' + '<input type="password" name="password" placeholder="password">' + '<input type="submit" value="login">' + '</form>')
+  res.send('<h1>Delta</h1>')
 }
