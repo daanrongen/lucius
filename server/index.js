c2e0e4c2e5cea09c1b076980191623e58e862543
daanrongen <daanrongen@hotmail.com>
2018-06-07 15:43:49 +0200

    clean up index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@
 import routes from '../src/routes/crm-routes'
 
 const express = require('express')
-const session = require('express-session')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
@@ -11,25 +10,12 @@ const app = express()
 const port = 4000
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost/CRMdb')
-
-routes(app)
+mongoose.connect('mongodb://localhost/delta')
 
 app
   .use(express.static('public'))
   .use(bodyParser.urlencoded({extended: true}))
   .use(bodyParser.json())
-  .use(session({
-    secret: 'secret',
-    resave: false,
-    saveUninitialized: false
-  }))
-  .set('view engine', 'ejs')
-  .set('views', 'public')
-  .get('/', all)
   .listen(port, console.log('listening on localhost:', port))
 
-function all(req, res) {
-  res.status(200)
-  res.send('<h1>Delta</h1>')
-}
+routes(app)
