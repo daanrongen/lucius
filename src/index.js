b680699038c1a9aa43601c11223b61a8d399affb
daanrongen <daanrongen@hotmail.com>
2018-09-12 20:43:28 +0200

    removed all previous node setup and started all over to build in react

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-import "./stylesheets/ui.scss";
-
-// window.React = React;
-//
-// render(<SkiDayCount />, document.getElementById("react-container"));
-
-ReactDOM.render(component, document.getElementById("location"));
