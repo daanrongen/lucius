fd1e98e2a19e3768c2181af4d433a7ccd1971e1d
daanrongen <daanrongen@hotmail.com>
2018-10-01 17:53:34 +0200

    css last page

--- momentum/src/components/BuoySettings.js
+++ momentum/src/components/BuoySettings.js
@@ -20,19 +20,27 @@ export class BuoySettings extends React.Component {
               <li>
                 <h2>buoy connection</h2>
               </li>
-              <li>{data.connectionStatus}</li>
+              <li>
+                <span>{data.connectionStatus}</span>
+              </li>
               <li>
                 <div id="connectionIndicator" />
               </li>
               <li>
-                <img src={cancelButton} alt="cancel button" />
+                <img src={cancelButton} alt="cancel button" id="cancelButton" />
               </li>
+            </ul>
+          </div>
+          <div id="buoyInformation">
+            <ul>
               <li>
                 <h2>led signal</h2>
               </li>
               <li>
                 <img id="buoyVisual" src={buoyVisual} alt="buoy visual" />
               </li>
+            </ul>
+            <ul>
               <li>
                 <h2>battery</h2>
               </li>
@@ -41,17 +49,22 @@ export class BuoySettings extends React.Component {
               </li>
               <li>
                 <h2>buoy id</h2>
+                <span>{data.buoyId}</span>
               </li>
-              <li>{data.buoyId}</li>
+            </ul>
+          </div>
+          <div id="doNotDisturb">
+            <ul>
               <li>
                 <h2>do not disturb mode</h2>
+                <span>{data.doNotDisturbMode}</span>
               </li>
-              <li>{data.doNotDisturbMode}</li>
             </ul>
-            <label class="switch">
+
+            {/* <label class="switch">
               <input type="checkbox" />
               <div class="slider" />
-            </label>
+            </label> */}
           </div>
         </article>
       </main>
